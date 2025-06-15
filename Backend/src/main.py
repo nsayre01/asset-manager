4a59e845a427f8f200ade1ba030027d9d48e9dbd
Kainanigames <kainanigames@gmail.com>
2025-06-15 16:27:04 -0700

    feat: Hooked up the frontend and backend so api calls can now be done on the website

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -6,8 +6,17 @@ from pydantic import BaseModel
 from datetime import date
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from models import Model
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="PLU Asset Manager")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Change to ["http://localhost:3000"] for more security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # DEFINE THE DATABASE CREDENTIALS
